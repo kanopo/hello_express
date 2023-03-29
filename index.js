f6ef7ecf26bf850386c081602c7836c82c18ef52
kanopo <dmitri.ollari@protonmail.com>
2023-03-29 18:20:14 +0200

    test listing file in s3 bucket

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-const express = require('express')
+import express from "express"
+import secrets from "secrets"
+import { S3Client, ListObjectsCommand } from "@aws-sdk/client-s3"
+
 const app = express()
 const port = 3000
 
+
+var bucketParams = {
+  Bucket: 'dmitri-bucket',
+};
+
+let command = new ListObjectsCommand(bucketParams)
+console.log(command)
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send(process.env.ACCESS_KEY)
 })
 
 app.listen(port, () => {
