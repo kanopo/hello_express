fac09e86f7d96bfac8d41e5651d2b4dc38808b2d
kanopo <dmitri.ollari@protonmail.com>
2023-04-12 10:46:12 +0200

    utilizzo chiave custom al posto delal default

--- scripts/npm.sh
+++ scripts/npm.sh
@@ -12,6 +12,6 @@
 # npm run build
 
 # ln -s /var/www/html/efs/.env /var/www/html/node_app/.env
-aws ssm get-parameter --name "dmitri-parameters" --with-decryption --query "Parameter.Value" | sed -e 's/\\n/\n/g' -e 's/\\"/"/g' -e 's/^"//' -e 's/"$//' > /var/www/html/node_app/.env
+aws ssm get-parameter --name "dmitri-parameters-cifrato" --with-decryption --query "Parameter.Value" | sed -e 's/\\n/\n/g' -e 's/\\"/"/g' -e 's/^"//' -e 's/"$//' > /var/www/html/node_app/.env
 sudo chown -R ec2-user:nginx /var/www/html/node_app
 
