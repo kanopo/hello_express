93bf077b625e33fa98dc686375b6280543dac50b
kanopo <dmitri.ollari@protonmail.com>
2023-04-12 09:36:49 +0200

    Modifica per usare aws system manager al posto di usare .env

--- scripts/npm.sh
+++ scripts/npm.sh
@@ -11,6 +11,7 @@
 # npm install
 # npm run build
 
-ln -s /var/www/html/efs/.env /var/www/html/node_app/.env
+# ln -s /var/www/html/efs/.env /var/www/html/node_app/.env
+aws ssm get-parameter --name "dmitri-parameters" --with-decryption --query "Parameter.Value" | sed -e 's/\\n/\n/g' -e 's/\\"/"/g' -e 's/^"//' -e 's/"$//' > /var/www/html/node_app/.env
 sudo chown -R ec2-user:nginx /var/www/html/node_app
 
